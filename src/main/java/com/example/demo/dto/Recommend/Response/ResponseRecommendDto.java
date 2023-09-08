e9f9bbde6a6927b5e38a619011f32b282b131bbf
zeun <real_jieun99@daum.net>
2023-09-08 14:32:43 +0900

    [#22] 음식 추천 응답 dto 수정

---
+++ src/main/java/com/example/demo/dto/Recommend/Response/ResponseRecommendDto.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto.Recommend.Response;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiOperation(
+        value = "음식 추천 응답",
+        notes = "사용자가 먹을 음식 목록을 추천한다.")
+public class ResponseRecommendDto {
+
+    private List<RecommendDto> recommendList = new ArrayList<>();
+}
