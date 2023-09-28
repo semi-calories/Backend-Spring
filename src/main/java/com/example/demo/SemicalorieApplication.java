19e38bb9f5e8ca4da69db2708a00bceb634111dd
zeun <real_jieun99@daum.net>
2023-09-28 23:40:51 +0900

    [#develop] ec2 시간차로 인한 문제 해결

--- src/main/java/com/example/demo/SemicalorieApplication.java
+++ src/main/java/com/example/demo/SemicalorieApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +9,19 @@ import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableJpaAuditing
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableRetry
 public class SemicalorieApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SemicalorieApplication.class, args);
 	}
